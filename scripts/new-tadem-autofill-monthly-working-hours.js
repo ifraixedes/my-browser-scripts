7ceb2aa7042ac7daba3b7ef858d07e85a7f025ef
Ivan Fraixedes <ivan@fraixed.es>
2021-01-05 17:39:45 +0100

    scripts: Modify New Tadem to ignore filled cells
    Modify the New Tandem script to not fill the input boxes that they have
    a value.

--- scripts/new-tadem-autofill-monthly-working-hours.js
+++ scripts/new-tadem-autofill-monthly-working-hours.js
@@ -28,9 +28,23 @@ function fillWorkDaysWithWorkingHours (dayRowsElems) {
 function fillWorkingHours (dayRowElem) {
   const inputs = dayRowElem.getElementsByTagName('input')
 
-  inputs[0].value = '09:00'
-  inputs[1].value = '14:00'
-  inputs[2].value = '15:00'
-  inputs[3].value = '18:00'
-  inputs[6].value = '8,00'
+  if (inputs[0].value = '') {
+    inputs[0].value = '09:00'
+  }
+
+  if (inputs[1].value = '') {
+    inputs[1].value = '14:00'
+  }
+
+  if (inputs[2].value = '') {
+    inputs[2].value = '15:00'
+  }
+
+  if (inputs[3].value = '') {
+    inputs[3].value = '18:00'
+  }
+
+  if (inputs[6].value = '') {
+    inputs[6].value = '8,00'
+  }
 }
